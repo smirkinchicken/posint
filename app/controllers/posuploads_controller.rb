653268bbafe6ae5b5d0e95862e35301605355676
michael johnson <mike@smirkinchicken.com>
2012-10-12 17:01:54 -0400

    mj

--- app/controllers/posuploads_controller.rb
+++ app/controllers/posuploads_controller.rb
@@ -2,24 +2,40 @@ class PosuploadsController < ApplicationController
   require 'aws/s3'
 
   def index
-    bucket = 'interview.playonsports.com'
-    @posfiles = AWS::S3::Bucket.find(bucket).objects
+    bucketname = 'interview.playonsports.com'
+    @posfiles = AWS::S3::Bucket.find(bucketname).objects
+    bucket = AWS::S3::Bucket.find(bucketname)
   end
 
     def upload
       begin
-        bucket = 'interview.playonsports.com'
-        AWS::S3::S3Object.store(sanitize_filename(params[:posfile].original_filename), params[:posfile].read, bucket, :access => :public_read)
-        redirect_to root_path
+        bucketname = 'interview.playonsports.com'
+        @posfiles = AWS::S3::Bucket.find(bucketname).objects
+        bucket = AWS::S3::Bucket.find(bucketname)
+        object = bucket.new_object
+
+        #AWS::S3::S3Object.store(params[:posfile].original_filename, File.open(params[:posfile].tempfile), bucketname)
+        object.value = File.open(params[:posfile].tempfile, 'r')
+        object.key   =   params[:posfile].original_filename
+         #   upfile = File.open(params[:posfile].tempfile, 'r')
+        object.store(:content_length => params[:posfile].size) do |buffer, bytes|
+          buffer.write(file.read(bytes))
+             end
+        end
+
+
+
+
+              redirect_to root_path
       #rescue
         #render :text => "Couldn't complete the upload"
 
       end
-    end
+
 
     private
     def sanitize_filename(file_name)
       just_filename = File.basename(file_name)
       just_filename.sub(/[^\w\.\-]/,'_')
     end
-  end
+      end
